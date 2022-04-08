5774ddbb2675cc4d5d1141dbe2b3efb8f064b114
zubaidr <web.expert1221@gmail.com>
2022-04-08 11:55:10 +0500

    updata data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,7 +10,7 @@ export class DataService {
   constructor(private http:HttpClient) { }
 
   getcurrencyData(){
-     return this.http.get<any>("https://v6.exchangerate-api.com/v6/65e12d857703c1673a8e23dc/latest/USD")
+     return this.http.get<any>("https://v6.exchangerate-api.com/v6/65e12d857703c1673a8e23dc/latest/PKR")
   }
 
 }
