863fc20d65da8c1134f67acb82f57ee43ecd2976
zubaidr <web.expert1221@gmail.com>
2022-04-12 12:02:49 +0500

    where update api

--- src/app/currencyApi.actions.ts
+++ src/app/currencyApi.actions.ts
@@ -2,6 +2,6 @@ import { createAction } from "@ngrx/store";
 import { props } from "@ngrx/store";
 // import { CurrencyItem } from "./currencyApi.states";
 
-export const LoadcurrenyApi=createAction('[currencyApi] LoadcurrenyApi')
+export const LoadcurrenyApi=createAction('[currencyApi] LoadcurrenyApi',props<any>())
 export const LoadSuccesscurrencyApi=createAction('[currencyApi] LoadSuccesscurrencyApi',
 props<any>())
