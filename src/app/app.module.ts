124b6d0bbd81dfc3e27f01e596644d2dc0348e17
zubaidr <web.expert1221@gmail.com>
2022-04-12 12:02:02 +0500

    udate a module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,29 +17,24 @@ import { TuiSvgModule } from '@taiga-ui/core';
 import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
 import { RateListComponent } from './rate-list/rate-list.component';
 import { TuiCardModule } from '@taiga-ui/addon-commerce';
-import { ReactiveFormsModule , FormsModule} from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { TuiInputModule } from '@taiga-ui/kit';
-
 import { TuiDropdownModule } from '@taiga-ui/core';
 import { TuiDataListModule } from '@taiga-ui/core';
 import { TuiDataListWrapperModule, TuiSelectModule } from '@taiga-ui/kit';
-import {TuiTableModule} from '@taiga-ui/addon-table';
-import {TuiInputNumberModule} from '@taiga-ui/kit';
-
-
+import { TuiTableModule } from '@taiga-ui/addon-table';
+import { TuiInputNumberModule } from '@taiga-ui/kit';
+import { TuiTablePaginationModule } from '@taiga-ui/addon-table';
+import {  TuiScrollbarModule } from '@taiga-ui/core';
 
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
 import { EffectsModule } from '@ngrx/effects';
 import { CurrencyApiEffects } from './currencyApi.effects';
 import { currencyReducer } from './currencyApi.reducer';
- 
-
-
 
 export class MyModule {}
 @NgModule({
-  declarations: [AppComponent,CurrencyConverterComponent,RateListComponent],
+  declarations: [AppComponent, CurrencyConverterComponent, RateListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -59,14 +54,14 @@ export class MyModule {}
     TuiSelectModule,
     TuiDataListModule,
     TuiDataListWrapperModule,
-    TuiTableModule,TuiInputNumberModule,
-    StoreModule.forRoot( {currency:currencyReducer
-    
-    }),
+    TuiTableModule,
+    TuiInputNumberModule,
+    TuiTablePaginationModule, TuiScrollbarModule,
+    StoreModule.forRoot({ currency: currencyReducer }),
     EffectsModule.forRoot([CurrencyApiEffects]),
-    EffectsModule.forFeature([CurrencyApiEffects])
+    EffectsModule.forFeature([CurrencyApiEffects]),
   ],
-  
+
   providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
   bootstrap: [AppComponent],
 })
