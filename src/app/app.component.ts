7a0d72f03a7b0285cfe0e3d35ce9fe82d50868a4
zubaidr <web.expert1221@gmail.com>
2022-04-12 12:01:04 +0500

    get data by store show in the dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable, retry } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { LoadcurrenyApi } from './currencyApi.actions';
 
 
 @Component({
@@ -14,13 +15,32 @@ export class AppComponent implements OnInit{
   activeItemIndex:any
   targetCurrency:any;
   getCurrencies:any[]=[];
+  getTargetValue:any
  
  
+
+  currencies$: Observable<any> = this.store.select((state) => {
+    return state.currency.currencies;
+  });
+
+  constructor(private store: Store<{ currency: { currencies: any[] } }>) {}
 
   ngOnInit(): void {
-   
+    this.store.dispatch(LoadcurrenyApi(null));
+    this.currencies$.subscribe((res) => {
+      this.getCurrencies= res;
+    });
   }
   targetvalue(item:any){
+    this.getTargetValue=item
+    console.log(this.targetCurrency)
+    this.store.dispatch(LoadcurrenyApi({data: this.targetCurrency}));
 
+    
+
+  }
+  addcurrency(){
+    this.targetCurrency.key
+   
   }
 }
